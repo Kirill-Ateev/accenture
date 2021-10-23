c62b68fba699449cbe72b391c433900a8c063856
BGDSHKA <kirill.ateev59@gmail.com>
2021-10-23 14:09:39 +0300

    chore

---
+++ src/components/Card/CardKpi/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CardKpi = ({title}) => {
+    return (
+        <div>
+            <Typography variant="body1">
+                
+            </Typography>
+        </div>
+    )
+}
+
+export default index
