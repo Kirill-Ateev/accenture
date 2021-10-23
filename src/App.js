967ca8e437740a9c691b384b670ec41aa9c46faa
BGDSHKA <kirill.ateev59@gmail.com>
2021-10-23 11:01:27 +0300

    chr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
-import Header from './components/Header'
+import { LinearProgress } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import { Route } from 'react-router-dom';
+import Header from './components/Header/Header'
+import { bindActionCreators } from 'redux';
+import * as userActions from './actions/user';
+import { connect } from 'react-redux';
 
-function App() {
+function App({ user, userActions }) {
+
+
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Header logout={userActions.logout} user={user} />
+      
+    </>
   );
 }
 
-export default App;
+function mapStateToProps() {
+  const mapStateToProps = state => {
+    return {
+      user: state.user
+    };
+  };
+  return mapStateToProps;
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    userActions: bindActionCreators(userActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,57 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState, useCallback } from 'react'
+import { useHistory } from 'react-router-dom';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import LoginModal from '../SigningModal/LoginModal';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
   },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
+  menuButton: {
+    marginRight: theme.spacing(2),
   },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  title: {
+    flexGrow: 1,
+    cursor: 'pointer',
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
@@ -51,39 +63,52 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export default function SearchAppBar() {
+const Header = ({ user, logout }) => {
+  const classes = useStyles();
+  const [drawer, setDrawer] = useState(false)
+  const history = useHistory();
+  const handleOnClick = useCallback(() => history.push('/'), [history]);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
+          <IconButton onClick={() => { setDrawer(true) }} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
+          <Typography onClick={handleOnClick} variant="h6" className={classes.title}>
             Accenture
           </Typography>
-          <Search>
-            <SearchIconWrapper>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
               <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
+            </div>
+            <InputBase
               placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
               inputProps={{ 'aria-label': 'search' }}
             />
-          </Search>
+          </div>
+          {user.isLogged ?
+            <Button color="inherit" onClick={logout}>Logout</Button>
+            : <Button color="inherit" onClick={handleOpen}>Sign in</Button>}
         </Toolbar>
       </AppBar>
-    </Box>
+      <LoginModal open={open} handleClose={handleClose} />
+    </div>
   );
 }
+
+export default Header;
